59bf9b6cf1b3f8b51e646319c74d16f7303b1a44
zmqp <81153204+prn-ic@users.noreply.github.com>
2022-07-11 13:43:51 +0300

    Shiiiiiiiiiiiiiiiiiiiiiiiiiit

---
+++ PersonParcer/PersonParcer/PersonParcer/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonParcer
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}; FirstName: {FirstName}; LastName: {LastName}; E-mail: {Email}; Age: {Age}; Profession: {Profession}";
+        }
+    }
+}
